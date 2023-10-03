797a213a1baf293e517e4762d98267b0bbc8e19d
entropicghost <145928051+entropicghost@users.noreply.github.com>
2023-10-04 00:28:42 +0100

    Add files via upload

---
+++ database.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('mydatabase.db')
+cursor = conn.cursor()
+
+cursor.execute()
+conn.close()
+
+conn = sqlite3.connect('mydatabase.db')
+cursor = conn.cursor()
+
+
+
+cursor.execute(
+    
+)
+
+conn.commit()
+conn.close()
---
+++ test.js
@@ -0,0 +1,4 @@
+//add leveling up system. each level gained increases the amount of
+//spooks and souls per second. levelling up is based on how many souls
+//spooks number always has to be higher than souls.
+//exp is gained from x amount of souls harvested
